3b45ed1e7056eee9cc3374e084f8c477a1602fd1
danny-reilly <daniel_reilly@students.nksd.net>
2025-03-18 15:59:51 -0400

    yeehaw
    old town road

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AAAstates/RegionalsAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AAAstates/RegionalsAuto.java
@@ -3,17 +3,17 @@ package org.firstinspires.ftc.teamcode.AAAstates;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.PinpointDrive;
 //import org.firstinspires.ftc.teamcode.SparkFunOTOSDrive;
-import org.firstinspires.ftc.teamcode.TankDrive;
 
+@Autonomous(name="RegionalsAuto", group="Robot")
 public final class RegionalsAuto extends LinearOpMode{
     @Override
-    public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(-63.5, -34, 270);
+    public void runOpMode(){
+        Pose2d beginPose = new Pose2d(-63.5, -34, DegToRad(270));
 
         PinpointDrive drive = new PinpointDrive(hardwareMap, beginPose);
 
@@ -21,7 +21,9 @@ public final class RegionalsAuto extends LinearOpMode{
 
         Actions.runBlocking(
                 drive.actionBuilder(beginPose)
-                        .splineTo(new Vector2d(-45, -45), DegToRad(315))
+                        .splineTo(new Vector2d(-63.5, -34), DegToRad(270))
+                        //.waitSeconds(0.25)
+                        //.splineTo(new Vector2d(-24, -24), DegToRad(315))
                         .build());
 
     }
