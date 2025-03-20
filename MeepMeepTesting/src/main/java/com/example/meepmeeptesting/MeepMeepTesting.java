c42ffc5e530713994370b54ee0d7c2399a044611
Dakspieth <jrb.games09@gmail.com>
2025-03-19 21:20:32 -0400

    ayyayayayayayayayayayayayyayayaa
    BANGG

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -11,14 +11,42 @@ public class MeepMeepTesting {
         MeepMeep meepMeep = new MeepMeep(800);
         Pose2d beginPose = new Pose2d(-63.5, -34, Math.toRadians(270));
 
+
+
+
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
 
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-63.5, -33, Math.toRadians(270)))
-                .splineTo(new Vector2d(-63.5, -34), Math.toRadians(270))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-33, -60, Math.toRadians(0)))
+
+                .setTangent(Math.toRadians(330))
+                .lineToY(-52)
+
+                .setReversed(true)
+                .splineTo(new Vector2d(-55, -55), Math.toRadians(225))
+
+                .setReversed(false)
+                .splineTo(new Vector2d(-48, -38), Math.toRadians(90))
+
+                .setReversed(true)
+                .splineTo(new Vector2d(-55, -55), Math.toRadians(225))
+
+                .setReversed(false)
+                .splineTo(new Vector2d(-58, -38), Math.toRadians(90))
+
+                .setReversed(true)
+                .splineTo(new Vector2d(-55, -55), Math.toRadians(225))
+
+                .setReversed(false)
+                .splineTo(new Vector2d(-52, -26), Math.toRadians(180))
+
+                .setReversed(true)
+                .splineTo(new Vector2d(-55, -55), Math.toRadians(225))
+
+
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
