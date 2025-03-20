66176e436f85c9d9977232b6d478dadad30c2a09
danny-reilly <daniel_reilly@students.nksd.net>
2025-03-20 14:36:07 -0400

    24341

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -15,17 +15,38 @@ public class MeepMeepTesting {
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
-                .build());
+                        .setTangent(Math.toRadians(330))
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                        .lineToY(-52)
+
+                        .setReversed(true)
+                        .splineTo(new Vector2d(-55, -55), Math.toRadians(225))
+
+                        .setReversed(false)
+                        .splineTo(new Vector2d(-48, -38), Math.toRadians(90))
+
+                        .setReversed(true)
+                        .splineTo(new Vector2d(-55, -55), Math.toRadians(225))
+
+                        .setReversed(false)
+                        .splineTo(new Vector2d(-58, -38), Math.toRadians(90))
+
+                        .setReversed(true)
+                        .splineTo(new Vector2d(-55, -55), Math.toRadians(225))
+
+                        .setReversed(false)
+                        .splineTo(new Vector2d(-52, -26), Math.toRadians(180))
+
+                        .setReversed(true)
+                        .splineTo(new Vector2d(-55, -55), Math.toRadians(225))
+
+
+                        .setReversed(false)
+                        .splineTo(new Vector2d(-46, -10), Math.toRadians(180))
+                        .strafeToSplineHeading(new Vector2d(-24, -12), Math.toRadians(180)));
+
+
+                meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -3,29 +3,70 @@ package org.firstinspires.ftc.teamcode.tuning;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.PinpointDrive;
-//import org.firstinspires.ftc.teamcode.SparkFunOTOSDrive;
+import org.firstinspires.ftc.teamcode.SparkFunOTOSDrive;
 import org.firstinspires.ftc.teamcode.TankDrive;
+import org.opencv.core.Mat;
 
+@Autonomous
 public final class SplineTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(-63.5, -34, 90);
-        if (TuningOpModes.DRIVE_CLASS.equals(PinpointDrive.class)) {
-            PinpointDrive drive = new PinpointDrive(hardwareMap, beginPose);
 
-            waitForStart();
+        Pose2d beginPose = new Pose2d(-33, -60, Math.toRadians(0));
 
-            Actions.runBlocking(
-                    drive.actionBuilder(beginPose)
-                            .splineTo(new Vector2d(-40 , -40), Math.PI / 2)
-                            //.splineTo(new Vector2d(0, 60), Math.PI)
-                            .build());
-        } else {
-            throw new RuntimeException();
-        }
+        //RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+        //.setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+
+        //.build();
+
+        //myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-33, -60, Math.toRadians(0)))
+
+
+        waitForStart();
+
+
+
+        PinpointDrive drive = new PinpointDrive(hardwareMap, beginPose);
+        Actions.runBlocking(
+                drive.actionBuilder(beginPose)
+
+                        .setTangent(Math.toRadians(330))
+
+                        .lineToY(-52)
+
+                        .setReversed(true)
+                        .splineTo(new Vector2d(-55, -55), Math.toRadians(225))
+
+                        .setReversed(false)
+                        .splineTo(new Vector2d(-48, -38), Math.toRadians(90))
+
+                        .setReversed(true)
+                        .splineTo(new Vector2d(-55, -55), Math.toRadians(225))
+
+                        .setReversed(false)
+                        .splineTo(new Vector2d(-58, -38), Math.toRadians(90))
+
+                        .setReversed(true)
+                        .splineTo(new Vector2d(-55, -55), Math.toRadians(225))
+
+                        .setReversed(false)
+                        .splineTo(new Vector2d(-52, -26), Math.toRadians(180))
+
+                        .setReversed(true)
+                        .splineTo(new Vector2d(-55, -55), Math.toRadians(225))
+
+
+                        .setReversed(false)
+                        .splineTo(new Vector2d(-46, -10), Math.toRadians(180))
+                        .strafeToSplineHeading(new Vector2d(-24, -12), Math.toRadians(180))
+
+
+                        .build());
     }
 }
